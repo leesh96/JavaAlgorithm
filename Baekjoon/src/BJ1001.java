8809ec6ff1e94ac1aa1f97184d06f08958a8e66b
leesh96 <suho2718@gmail.com>
2021-07-16 01:30:14 +0900

    리포지토리 정리

--- Baekjoon/src/BJ1001.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-
-public class BJ1001 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println(sc.nextInt() - sc.nextInt());
-    }
-}
