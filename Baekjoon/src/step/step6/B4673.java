4946b28d6527cb49dda1b8aa6eb8747fae04798a
leesh96 <suho2718@gmail.com>
2021-08-15 19:22:05 +0900

    백준 단계별로 풀어보기 ~7단계

---
+++ Baekjoon/src/step/step6/B4673.java
@@ -0,0 +1,35 @@
+package step.step6;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class B4673 {
+    static boolean[] arr;
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        arr = new boolean[10001];
+        for (int i = 1; i <= 10000; i++) {
+            int temp = d(i);
+            if (temp < 10001) {
+                arr[d(i)] = true;
+            }
+        }
+        for (int i = 1; i <= 10000; i++) {
+            if (!arr[i]) {
+                bw.write(i + "\n");
+            }
+        }
+        bw.flush();
+        bw.close();
+    }
+
+    public static int d(int n) {
+        int sum = n;
+        while (n != 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum;
+    }
+}
