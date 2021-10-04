40cd99b4ceb73fe7fcb553173f625ff0a2eb6cb0
leesh96 <suho2718@gmail.com>
2021-10-04 20:11:23 +0900

    [Solve] 21.10.04. 백준 Solved.ac 클래스 3 1문제 풀이
    백준
    11403 경로 찾기
    
    + 테스트 문제 주석 안단것 달았음

--- src/baekjoon/test0928/B14921.java
+++ src/baekjoon/test0928/B14921.java
@@ -3,6 +3,23 @@ package baekjoon.test0928;
 import java.io.*;
 import java.util.StringTokenizer;
 
+/*
+플랫폼 : 백준
+문제번호 : 14921
+문제제목 : 용액 합성하기
+난이도 : 골드 5
+제한사항 : 1초/512MB
+알고리즘 분류 : 투 포인터
+
+알고리즘 설명
+1. 양 끝점으로 투 포인터 탐색
+2. 현재 두 포인터가 가르키는 용액의 합의 절대값이 더 작은것으로 갱신
+3. 합이 0보다 크면 우측 포인터 감소, 합이 0보다 작으면 왼쪽 포인터 증가
+
+채점 결과 : 396ms/25524KB
+풀이 날짜 : 2021/10/04
+*/
+
 public class B14921 {
     static int[] arr;
     static int n;
--- src/baekjoon/test0928/B1890.java
+++ src/baekjoon/test0928/B1890.java
@@ -3,6 +3,24 @@ package baekjoon.test0928;
 import java.io.*;
 import java.util.StringTokenizer;
 
+/*
+플랫폼 : 백준
+문제번호 : 1890
+문제제목 : 점프
+난이도 : 실버 2
+제한사항 : 1초/128MB
+알고리즘 분류 : DP
+
+알고리즘 설명
+1. DFS로 풀 경우 시간초과
+2. 각 점에서 오른쪽 또는 아래로 그 수만큼 이동할 수 있으므로
+3. 시작점(0, 0)을 1로 만들어 놓고 갈 수 있는 곳에 이전 점의 숫자를 더해준다.
+4. 최종적으로 끝 점엔 경로의 수가 저장되어있음
+
+채점 결과 : 152ms/14384KB
+풀이 날짜 : 2021/10/04
+*/
+
 public class B1890 {
     static int[][] pane;
     static int n;
