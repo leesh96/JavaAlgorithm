6d26e893abfe16045d2e5435dcb0f4907628eef9
leesh96 <suho2718@gmail.com>
2021-10-18 22:31:20 +0900

    [Solve] 21.10.18. 백준 Solved.ac 클래스 4 4문제 풀이
    백준
    15650 N과 M (2)
    15652 N과 M (4)
    15654 N과 M (5)
    15657 N과 M (8)

---
+++ src/baekjoon/class4/B15654.java
@@ -0,0 +1,68 @@
+package baekjoon.class4;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/*
+플랫폼 : 백준
+문제번호 : 15654
+문제제목 : N과 M (5)
+난이도 : 실버 3
+제한사항 : 1초/512MB
+알고리즘 분류 : 백트래킹
+
+알고리즘 설명
+1. 백트래킹
+2. 순열 구하기
+
+채점 결과 : 424ms/37124KB
+풀이 날짜 : 2021/10/18
+*/
+
+public class B15654 {
+    static int n, m;
+    static int[] arr;
+    static boolean[] visit;
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        visit = new boolean[n];
+        Arrays.sort(arr);
+
+        dfs(new int[m], 0);
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    static void dfs(int[] ans, int depth) throws IOException {
+        if (depth == m) {
+            for (int i = 0; i < m; i++) {
+                bw.write(ans[i] + " ");
+            }
+            bw.newLine();
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (!visit[i]) {
+                visit[i] = true;
+                ans[depth] = arr[i];
+                dfs(ans, depth + 1);
+                visit[i] = false;
+            }
+        }
+    }
+}
---
+++ src/baekjoon/class4/B15657.java
@@ -0,0 +1,62 @@
+package baekjoon.class4;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/*
+플랫폼 : 백준
+문제번호 : 15657
+문제제목 : N과 M (8)
+난이도 : 실버 3
+제한사항 : 1초/512MB
+알고리즘 분류 : 백트래킹
+
+알고리즘 설명
+1. 백트래킹
+2. 중복 조합 구하기
+
+채점 결과 : 228ms/18108KB
+풀이 날짜 : 2021/10/18
+*/
+
+public class B15657 {
+    static int n, m;
+    static int[] arr;
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        dfs(new int[m], 0, 0);
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    static void dfs(int[] ans, int depth, int cur) throws IOException {
+        if (depth == m) {
+            for (int i = 0; i < m; i++) {
+                bw.write(ans[i] + " ");
+            }
+            bw.newLine();
+            return;
+        }
+
+        for (int i = cur; i < n; i++) {
+            ans[depth] = arr[i];
+            dfs(ans, depth + 1, i);
+        }
+    }
+}
