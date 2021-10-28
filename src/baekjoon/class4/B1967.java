4060d7f54a6dad3292a9a3a82711d45a886f28c0
leesh96 <suho2718@gmail.com>
2021-10-28 13:55:34 +0900

    [Solve] 21.10.28. 백준 Solved.ac 클래스 4 1문제 풀이
    백준
    1967 트리의 지름

--- src/baekjoon/class4/B1967.java
+++ src/baekjoon/class4/B1967.java
@@ -4,6 +4,21 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
+/*
+플랫폼 : 백준
+문제번호 : 1967
+문제제목 : 트리의 지름
+난이도 : 골드 4
+제한사항 : 2초/128MB
+알고리즘 분류 : 그래프 이론, 그래프 탐색, 트리, DFS
+
+알고리즘 설명
+DFS로 가장 긴 경로의 노드 찾고 해당 노드에서 한 번 더 DFS
+
+채점 결과 : 236ms/23268KB
+풀이 날짜 : 2021/10/28
+*/
+
 public class B1967 {
     static int n;
     static boolean[] visit;
