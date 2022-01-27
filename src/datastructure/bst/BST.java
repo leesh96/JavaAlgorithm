5e389cc69e02bcaad2961861cad55f9b43f9b849
leesh96 <suho2718@gmail.com>
2022-01-27 11:54:57 +0900

    [DataStructure] 22.01.27. 이진 탐색 트리(2)
    삭제 연산 구현

--- src/datastructure/bst/BST.java
+++ src/datastructure/bst/BST.java
@@ -38,6 +38,49 @@ public class BST {
         else parent.right = newNode;
     }
 
+    public boolean remove(int value) {
+        Node parent = root;
+        Node target = root;
+        boolean isLeft = false;
+
+        while (target.value != value) {
+            parent = target;
+            if (value < target.value) {
+                isLeft = true;
+                target = target.left;
+            } else {
+                isLeft = false;
+                target = target.right;
+            }
+            if (target == null) {
+                return false;
+            }
+        }
+
+        if (target == root) root = delete(target);
+        else if (isLeft) parent.left = delete(target);
+        else parent.right = delete(target);
+        return true;
+    }
+
+    private Node delete(Node target) {
+        if (target.left == null && target.right == null) return null;
+        else if (target.left == null) return target.right;
+        else if (target.right == null) return target.left;
+        else {
+            Node parent = target;
+            Node next = target.right;
+            while (next.left != null) {
+                parent = next;
+                next = next.left;
+            }
+            target.value = next.value;
+            if (next == target.right) target.right = next.right;
+            else parent.left = next.right;
+            return target;
+        }
+    }
+
     public void inOrder(Node root) {
         if (root != null) {
             inOrder(root.left);
